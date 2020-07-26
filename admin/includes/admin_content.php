47c15462b657c414faf463de49582eb5ad9a3809
Roshan Elvitigala <roshanrox911@gmail.com>
2020-07-26 18:01:51 +1000

    added get user by id method

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -8,13 +8,13 @@
                             <small>Subheading</small>
                         </h1>
                          <?php 
-                                $user = new User();
-                                $result_set = $user->find_all_users();
+                                $result_set = User::find_all_users();
                                 while($row = mysqli_fetch_array($result_set)){
                                     echo $row['username']."<br>";
                                 }
                                 
-                                
+                                $found_user = User::find_user_by_id(2);
+                                echo $found_user['username'];
                             ?>
                         <ol class="breadcrumb">
                             <li>
