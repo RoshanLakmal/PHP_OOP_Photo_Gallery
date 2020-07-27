53e4343681d29f0f50c4e708119a473df3891fba
Roshan Elvitigala <roshanrox911@gmail.com>
2020-07-27 11:04:47 +1000

    added message method for session

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -4,10 +4,29 @@ class Session {
 
 	private $signed_in = false;
 	public $user_id;
+	public $message;
 
 	function __construct(){
 		session_start();
 		$this->check_the_login();
+		$this->check_message();
+	}
+
+	public function message($msg=""){
+		if(!empty($msg)){
+			$_SESSION['message'] = $msg;
+		}else{
+			return $this->message;
+		}
+	}
+
+	private function check_message(){
+		if(isset($_SESSION['message'])){
+			$this->message = $_SESSION['message'];
+			unset($_SESSION['message']);
+		}else{
+			$this->message="";
+		}
 	}
 
 	private function check_the_login(){
@@ -19,6 +38,8 @@ class Session {
 			$this->signed_in=false;
 		}
 	}
+
+
 
 	public function is_signed_in(){
 		return $this->signed_in;
